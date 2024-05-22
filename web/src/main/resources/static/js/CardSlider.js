92190845c03c867ebd1283d2ce7025f2ce74267e
choidh <choidh.dev@gmail.com>
2024-05-22 10:06:05 +0900

    card slider.

---
+++ web/src/main/resources/static/js/CardSlider.js
@@ -0,0 +1,68 @@
+const CardSlider = (() => {
+    let that = {};
+
+    let isDragging = false;
+    let startPos = 0;
+    let currentTranslate = 0;
+    let prevTranslate = 0;
+    let animationID = 0;
+
+    let slider;
+    let sliderTrack;
+
+    that.init = () => {
+        slider = document.getElementById('recommend-learning-item-container');
+        sliderTrack = document.getElementById('recommend-learning-item-wrapper');
+
+        slider.addEventListener('mousedown', dragStart);
+        slider.addEventListener('mouseup', dragEnd);
+        slider.addEventListener('mouseleave', dragEnd);
+        slider.addEventListener('mousemove', drag);
+
+        slider.addEventListener('touchstart', dragStart);
+        slider.addEventListener('touchend', dragEnd);
+        slider.addEventListener('touchmove', drag);
+    }
+
+    function dragStart(event) {
+        isDragging = true;
+        startPos = getPositionX(event);
+        animationID = requestAnimationFrame(animation);
+        sliderTrack.style.transition = 'none';
+    }
+
+    function drag(event) {
+        if (isDragging) {
+            const currentPosition = getPositionX(event);
+            currentTranslate = (prevTranslate + currentPosition) - startPos;
+        }
+    }
+
+    function dragEnd() {
+        isDragging = false;
+        cancelAnimationFrame(animationID);
+
+        if (currentTranslate > 0) currentTranslate = 0;
+        else if (currentTranslate < -2200) currentTranslate = -2200;
+
+        setSliderPosition();
+        prevTranslate = currentTranslate;
+
+        sliderTrack.style.transition = 'transform 0.3s ease';
+    }
+
+    function getPositionX(event) {
+        return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+    }
+
+    function animation() {
+        setSliderPosition();
+        if (isDragging) requestAnimationFrame(animation);
+    }
+
+    function setSliderPosition() {
+        sliderTrack.style.transform = `translateX(${currentTranslate}px)`;
+    }
+
+    return that;
+})()
