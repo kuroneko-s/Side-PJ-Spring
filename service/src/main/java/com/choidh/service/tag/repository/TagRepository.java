d70565d74d6018983291683319711d2d90065739
choidh <choidh.dev@gmail.com>
2024-06-05 10:14:16 +0900

    Tag Repository 정리.

--- service/src/main/java/com/choidh/service/tag/repository/TagRepository.java
+++ service/src/main/java/com/choidh/service/tag/repository/TagRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Tag findByTitle(String title);
-
-    void deleteByTitle(String title);
-
-    boolean existsByTitle(String title);
 }
