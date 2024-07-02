840ba11ba46a9b7764cb9d22615baaeb3747e0cb
choidh <choidh.dev@gmail.com>
2024-07-02 15:53:50 +0900

    로그인 성공 핸들러 수정.

--- web/src/main/java/com/choidh/web/common/config/SecurityConfig.java
+++ web/src/main/java/com/choidh/web/common/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.choidh.web.common.config;
 import com.choidh.service.security.AccountDetailsService;
 import com.choidh.service.security.AccountRoleType;
 import com.choidh.service.security.CustomAuthenticationFailureHandler;
-import com.choidh.service.security.CustomAuthenticationProvider;
+import com.choidh.service.security.CustomAuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,18 +15,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final DataSource dataSource;
     private final AccountDetailsService accountDetailsService;
     private final CustomAuthenticationFailureHandler authenticationFailureHandler;
-    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
 
     // 바로 접근 가능한 URL
     @Value("${security.accessible.url}")
@@ -53,15 +46,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.formLogin()
-            .loginPage("/login")
-            .failureHandler(authenticationFailureHandler)
-            .successHandler(new AuthenticationSuccessHandler() {
-                @Override
-                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                    redirectStrategy.sendRedirect(httpServletRequest, httpServletResponse, "/");
-                }
-            })
-            .permitAll();
+                .loginPage("/login")
+                .failureHandler(authenticationFailureHandler)
+                .successHandler(authenticationSuccessHandler)
+                .permitAll()
+                .and()
+                .requestCache()
+                .requestCache(new HttpSessionRequestCache());
 
         http.logout()
                 .logoutUrl("/logout")
